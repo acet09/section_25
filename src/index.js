14c495744f07105279730a9fe0d187f0eadb48d1
Cactus <ysjungman@gmail.com>
2023-06-13 00:07:35 +0900

    pull-0613

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+
+import './index.css';
+import App from './App';
+import ProductsProvider from './context/products-context'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// );
